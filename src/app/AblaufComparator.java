5a3d3fb9cabb2814e6809d93afaa94004687447f
Philipp(PC) <philipp.schmeier@gmail.com>
2017-06-27 21:43:16 +0200

    init aufg3 teil2

--- src/app/AblaufComparator.java
+++ src/app/AblaufComparator.java
@@ -9,6 +9,14 @@ import java.util.Comparator;
 public class AblaufComparator implements Comparator<Maschine> {
 
 	@Override
+	/**
+	 * In der Klasse AblaufComparator sollen die Maschinen so verglichen werden,
+	 * dass eine logische Abfolge realisiert werden kann. Wird dieser Comparator der sort()-Methode der Maschinenliste übergeben,
+	 * so soll später (soweit möglich) jede Maschine ausführbar sein
+	 * (also im Falle eines Verwerters alle Abhängigkeiten vorhanden sein).
+	 * Bedenken Sie hierbei die unterschiedlichen Typen der Maschinen sowie deren Erzeugnisse und Abhängigkeiten.
+	 * Folgende Bilder zeigen ein mögliches Ergebnis dieses Sortiervorganges:
+	 */
 	public int compare(Maschine o1, Maschine o2) {
 		// TODO Auto-generated method stub
 		return 0;
